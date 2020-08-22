66fb3f12fae46774f4cc1ad66e6f9df025f5b267
D@RK_H04X_FR33D0M <33440266+DarkHoax@users.noreply.github.com>
2020-08-22 00:20:04 -0300

    Update lista01_ex11.py

--- lista01_ex11.py
+++ lista01_ex11.py
@@ -3,12 +3,12 @@
 '''Created on Wed Jan  8 00:03:06 2020
 
 lista 01 - QUESTÃO 11. Crie um algoritmo que calcule a soma dos N primeiros 
-números inteiros ímpares e positivos. O valor de N deve ser lido do usuário.
+números inteiros ímpares e positivos. O valor de N deve ser lido do usuário.'''
 
-@author: argos'''
-soma = 0
+soma = i = 0
 n = int(input('Digite um número: '))
-for i in range(0, n + 1):
+while i != n:
+    i += 1    
     if i > 0:
         if i % 2 == 1:
             soma += i
